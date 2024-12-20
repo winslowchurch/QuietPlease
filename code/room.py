bd0ddc58070b9c6df51cb729b6d91282dc1d1f01
Winslow Church <winslowc@stanford.edu>
2024-12-20 09:32:11 -0800

    decoration

--- code/room.py
+++ code/room.py
@@ -8,6 +8,7 @@ class Room:
 		self.libraryRoom = Library_Room(self.ui)
 		self.current = self.libraryRoom # initial room
 		
+	### ALL Room Classes will have an update based on clicks function ###
 	def update(self, clickedCoordinates): 
 		self.current.update(self, clickedCoordinates)
 
@@ -20,7 +21,8 @@ class Library_Room:
 		self.counter = pygame.image.load(f'{overlay_path}counter.png').convert_alpha()
 		self.walls = pygame.image.load(f'{overlay_path}walls.png').convert_alpha()
 		self.floor = pygame.image.load(f'{overlay_path}floor.png').convert_alpha()
-		
+		self.decoration = pygame.image.load(f'{overlay_path}decoration.png').convert_alpha()
+
 		self.outside = Outside(ui.display_surface)
 
 	def update(self, Room, mp):
@@ -30,6 +32,7 @@ class Library_Room:
 		self.outside.draw(surface, day_timer)
 		surface.blit(self.floor, (0,0))
 		surface.blit(self.walls, (0,0))
+		surface.blit(self.decoration, (0,0))
 		surface.blit(self.counter,(0,0))
 
 		self.ui.showBookBucks()
