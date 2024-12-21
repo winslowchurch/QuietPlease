f1ec6d30dd3b00f02d95ce10ae2f5c8a650d4229
Winslow Church <winslowc@stanford.edu>
2024-12-21 08:25:29 -0800

    transition out

--- code/customer.py
+++ code/customer.py
@@ -15,28 +15,36 @@ class Customer:
         }
 
         # Transition-related attributes
-        self.transitionY = SCREEN_HEIGHT  # Start from below the screen
-        self.transitionSpeed = -10  # Negative speed to move upward
+        self.transitionY = 0  # Controls the movement of the customer
+        self.transitionSpeed = 10  # Speed of transition
         self.inTransition = False
+        self.transitionPhase = None  # "outgoing" or "incoming"
         self.newCustomerData = None  # Holds the data for the incoming customer
 
     def draw(self, surface, ui):
         if self.inTransition:
-            # During transition, draw both old and new customers
-            if self.current:
-                surface.blit(self.current, (0, self.transitionY + SCREEN_HEIGHT))
-            if self.newCustomerData:
-                surface.blit(self.newCustomerData['surface'], (0, self.transitionY))
-            
-            # Move the transition position
-            self.transitionY += self.transitionSpeed
+            if self.transitionPhase == "outgoing":
+                # Move the old customer down the screen
+                if self.current:
+                    surface.blit(self.current, (0, self.transitionY))
+                self.transitionY += self.transitionSpeed
 
-            # If the transition is complete, finalize the switch
-            if self.transitionY <= 0:  # Stop when the new customer reaches y = 0
-                self.current = self.newCustomerData['surface']
-                self.currentDialogue = self.newCustomerData['dialogue']
-                self.transitionY = SCREEN_HEIGHT  # Reset for the next transition
-                self.inTransition = False
+                # Check if the old customer has fully exited the screen
+                if self.transitionY >= surface.get_height():
+                    self.transitionPhase = "incoming"
+                    self.transitionY = surface.get_height()  # Start new customer below the screen
+            elif self.transitionPhase == "incoming":
+                # Move the new customer up the screen
+                if self.newCustomerData:
+                    surface.blit(self.newCustomerData['surface'], (0, self.transitionY))
+                self.transitionY -= self.transitionSpeed
+
+                # Check if the new customer has reached their normal position
+                if self.transitionY <= 0:
+                    self.current = self.newCustomerData['surface']
+                    self.currentDialogue = self.newCustomerData['dialogue']
+                    self.transitionY = 0
+                    self.inTransition = False
         else:
             # Regular drawing if no transition is happening
             if self.current:
@@ -48,16 +56,17 @@ class Customer:
         if self.inTransition:
             return  # Prevent starting a new transition if one is already happening
 
+        # Set up the new customer data
         customer = random.choice(list(CUSTOMERS_LIST.keys()))
         newSurface = self.surfOptions.get(customer)
         newDialogue = random.choice(CHECKOUT_DIALOGUE_LIST)
 
-        # Set up the new customer data
         self.newCustomerData = {
             'surface': newSurface,
             'dialogue': newDialogue
         }
 
-        # Start the transition
-        self.transitionY = SCREEN_HEIGHT  # Start from below the screen
+        # Start the outgoing transition for the current customer
+        self.transitionY = 0  # Start from the current position
+        self.transitionPhase = "outgoing"
         self.inTransition = True
