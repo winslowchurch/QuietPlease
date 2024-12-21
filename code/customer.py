2fe0afd8af2779d62e22546775f7d21d9b1255f8
Winslow Church <winslowc@stanford.edu>
2024-12-21 08:23:30 -0800

    transition

--- code/customer.py
+++ code/customer.py
@@ -1,6 +1,7 @@
 import pygame
-from data import * 
+from data import *
 import random
+from settings import *
 
 class Customer:
     def __init__(self):
@@ -13,16 +14,50 @@ class Customer:
             for name, filename in CUSTOMERS_LIST.items()
         }
 
-    ### Draws the current customer ***
+        # Transition-related attributes
+        self.transitionY = SCREEN_HEIGHT  # Start from below the screen
+        self.transitionSpeed = -10  # Negative speed to move upward
+        self.inTransition = False
+        self.newCustomerData = None  # Holds the data for the incoming customer
+
     def draw(self, surface, ui):
-        if self.current:
-            surface.blit(self.current, (0, 0))
-        if self.currentDialogue:
-            self.checkoutButtonRect = ui.showDialogue(self.currentDialogue)
-    
-    ### Randomly selects new customer and question
+        if self.inTransition:
+            # During transition, draw both old and new customers
+            if self.current:
+                surface.blit(self.current, (0, self.transitionY + SCREEN_HEIGHT))
+            if self.newCustomerData:
+                surface.blit(self.newCustomerData['surface'], (0, self.transitionY))
+            
+            # Move the transition position
+            self.transitionY += self.transitionSpeed
+
+            # If the transition is complete, finalize the switch
+            if self.transitionY <= 0:  # Stop when the new customer reaches y = 0
+                self.current = self.newCustomerData['surface']
+                self.currentDialogue = self.newCustomerData['dialogue']
+                self.transitionY = SCREEN_HEIGHT  # Reset for the next transition
+                self.inTransition = False
+        else:
+            # Regular drawing if no transition is happening
+            if self.current:
+                surface.blit(self.current, (0, 0))
+            if self.currentDialogue:
+                self.checkoutButtonRect = ui.showDialogue(self.currentDialogue)
+
     def newCustomer(self):
+        if self.inTransition:
+            return  # Prevent starting a new transition if one is already happening
+
         customer = random.choice(list(CUSTOMERS_LIST.keys()))
-        self.current = self.surfOptions.get(customer)
-        
-        self.currentDialogue = random.choice(CHECKOUT_DIALOGUE_LIST)
+        newSurface = self.surfOptions.get(customer)
+        newDialogue = random.choice(CHECKOUT_DIALOGUE_LIST)
+
+        # Set up the new customer data
+        self.newCustomerData = {
+            'surface': newSurface,
+            'dialogue': newDialogue
+        }
+
+        # Start the transition
+        self.transitionY = SCREEN_HEIGHT  # Start from below the screen
+        self.inTransition = True
